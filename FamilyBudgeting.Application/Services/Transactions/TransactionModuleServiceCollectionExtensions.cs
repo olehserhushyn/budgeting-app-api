e6bcfa26727199160f8ab1272dd7ec91c93a1a56
Oleh Serhushyn <olehserhushyn.job@gmail.com>
2026-03-03 19:28:34 +0000

    refactor: split transaction module DI into policies/commands/queries

--- FamilyBudgeting.Application/Services/Transactions/TransactionModuleServiceCollectionExtensions.cs
+++ FamilyBudgeting.Application/Services/Transactions/TransactionModuleServiceCollectionExtensions.cs
@@ -8,12 +8,33 @@ namespace FamilyBudgeting.Domain.Services
         public static IServiceCollection AddTransactionModule(this IServiceCollection services)
         {
             services.AddScoped<ITransactionService, TransactionService>();
+
+            services.AddTransactionPolicies();
+            services.AddTransactionCommandHandlers();
+            services.AddTransactionQueryHandlers();
+
+            return services;
+        }
+
+        public static IServiceCollection AddTransactionPolicies(this IServiceCollection services)
+        {
             services.AddScoped<ITransactionAccessPolicy, TransactionAccessPolicy>();
+            return services;
+        }
+
+        public static IServiceCollection AddTransactionCommandHandlers(this IServiceCollection services)
+        {
             services.AddScoped<ITransactionCreateHandler, TransactionCreateHandler>();
             services.AddScoped<ITransactionUpdateHandler, TransactionUpdateHandler>();
             services.AddScoped<ITransactionDeleteHandler, TransactionDeleteHandler>();
             services.AddScoped<ITransactionTransferHandler, TransactionTransferHandler>();
             services.AddScoped<ITransactionImportHandler, TransactionImportHandler>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddTransactionQueryHandlers(this IServiceCollection services)
+        {
             services.AddScoped<ITransactionListQueryHandler, TransactionListQueryHandler>();
             services.AddScoped<ITransactionPageDataQueryHandler, TransactionPageDataQueryHandler>();
 
